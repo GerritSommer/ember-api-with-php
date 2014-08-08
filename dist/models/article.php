66cb838f70cd2c97674011a1cd69367e9b10f038
Gerrit Sommer <gerrit.sommer@outlook.com>
2014-08-08 17:41:21 +0200

    removed dist folder

--- dist/models/article.php
+++
@@ -1,2 +0,0 @@
-<?php
-class Article extends ActiveRecord\Model {}
--- dist/models/home.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Home extends ActiveRecord\Model {
-  static $table_name = 'home';
-  // static $validates_presence_of = array(array('name'), array('role'));
-}
