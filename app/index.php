63110f61b47dc672fc75f80a218bade78410f247
Gerrit Sommer <gerrit.sommer@outlook.com>
2014-08-08 17:41:31 +0200

    gitignore

--- app/index.php
+++ app/index.php
@@ -1,7 +1,11 @@
 <?php
+
   require_once 'includes/initialize.php';
   require_once getcwd().'/../config/app_config.php';
-?>
+    ini_set('display_errors',1);
+    ini_set('display_startup_errors',1);
+    error_reporting(-1);
+  ?>
   <!DOCTYPE html>
   <html>
     <head>
@@ -9,6 +13,7 @@
     </head>
   <body>
     <?php
+    test
       require_once 'router.php';
     ?>
   </body>
