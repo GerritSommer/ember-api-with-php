3dc17b3b673a6164d32c226e84e13c710d967c52
Gerrit Sommer <gerrit.sommer@outlook.com>
2014-08-10 17:09:16 +0200

    removed html

--- app/router.php
+++ app/router.php
@@ -1,45 +1,31 @@
 <?php
 require_once 'models/user.php';
+require_once 'controllers/users_controller.php';
+$app->response()->header('Content-Type', 'application/json');
+$app->response()->header('Content-Type', 'application/json');
+$app->response()->header('Content-Type', 'application/json');
+header('Access-Control-Allow-Origin: *');
 
-$app->get('/', function () use ($app) {
-    require_once 'models/home.php';
+
+$app->get('/users/', function () use ($app) {
+  $controller = new UsersController;
+  $json = $controller->index();
+
+  echo $json;
 });
 
-$app->get('/blog/', function () use ($app) {
-  require_once 'controllers/articles_controller.php';
-  require_once 'models/article.php';
-  $controller = new ArticlesController;
+$app->get('/users/:id', function ($id) use ($app) {
+  $controller = new UsersController;
+  $json = $controller->show($id);
+
+  echo $json;
 });
 
-$app->get('/login/', function () use ($app) {
-
-});
-
-$app->post('/login/', function () use ($app) {
-  require_once 'controllers/session_controller.php';
-  $controller = new SessionController;
-  $post       = $app->request()->post();
-  $user       = $controller->validate($post);
-
-  if($user) {
-    $_Session['user'] = $user;
-    $app->redirect('/');
-  } else {
-    $app->redirect('/login');
-    // error messages
-  }
-});
-
-$app->get('/admin', function () use ($app) {
-});
-
-$app->get('/admin/dashboard', function () use ($app) {
-});
-
-$app->get('/admin/home', function () use ($app) {
-  require_once 'models/home.php';
-  $home = Home::find('first');
-});
-
+// $app->get('/:param1(/:param2(/:param3))', function () use ($app) {
+//     $args = func_get_args();
+//     foreach($args as $arg){
+//         echo $arg . ' -- ';
+//     }
+// });
 
 $app->run();
