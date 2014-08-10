1d89b18436ed4ffcaedd6ee1b3d16306f0e92884
Gerrit Sommer <gerrit.sommer@outlook.com>
2014-08-10 17:09:31 +0200

    removed html

--- app/index.php
+++ app/index.php
@@ -1,20 +1,12 @@
 <?php
 
-  require_once 'includes/initialize.php';
-  require_once getcwd().'/../config/app_config.php';
+    require_once 'includes/initialize.php';
+    require_once getcwd().'/../config/app_config.php';
     ini_set('display_errors',1);
     ini_set('display_startup_errors',1);
     error_reporting(-1);
   ?>
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="UTF-8">
-    </head>
-  <body>
+
     <?php
-    test
       require_once 'router.php';
     ?>
-  </body>
-</html>
