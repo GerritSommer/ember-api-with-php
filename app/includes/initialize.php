7ba218c49fe2cbfc342c93dc4d0bfcf8f563dd00
Gerrit Sommer <gerrit.sommer@outlook.com>
2014-08-10 17:09:51 +0200

    db

--- app/includes/initialize.php
+++ app/includes/initialize.php
@@ -10,10 +10,12 @@ ini_set('short_open_tag', 'On');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASSWD', '');
-define('DB_NAME', 'dirks');
+define('DB_NAME', 'base');
+
 
 // Active Record
 require_once 'includes/php-activerecord/ActiveRecord.php';
+
 ActiveRecord\Config::initialize(function($cfg) {
     $cfg->set_model_directory('models');
     $cfg->set_connections(array( 'development' => 'mysql://'.DB_USER.'@'.DB_HOST.'/'.DB_NAME));
